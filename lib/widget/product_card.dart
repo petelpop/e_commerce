7232d5b55c8c9f4ba7c537702ceb9d2ad18958ae
ipal <72238450+petelpop@users.noreply.github.com>
2024-05-20 14:36:07 +0700

    fix product card ui

--- lib/widget/product_card.dart
+++ lib/widget/product_card.dart
@@ -17,26 +17,54 @@ class ProductCard extends StatelessWidget {
           width: double.infinity,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(20),
-            color: Color(0xffF5F5F5)
+            color: const Color(0xffF5F5F5)
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 15),
+              const SizedBox(height: 15),
               Center(
-                child: Image.asset(product.image, width: 130, height: 130, fit: BoxFit.cover,),
+                child: Image.asset(product.image, width: 110, height: 110, fit: BoxFit.cover,),
               ),
-              SizedBox(height: 10),
+              const SizedBox(height: 10),
               Padding(
-                padding: EdgeInsets.only(left: 10),
+                padding: const EdgeInsets.only(left: 10),
                 child: Text(
                   product.title,
-                  style: TextStyle(
+                  style: const TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 16
                   ),
                 ),
                 ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                  "\$${product.price}",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17,
+                  ),
+                ),
+                Row(
+                  children: 
+                    List.generate(
+                      product.colors.length, 
+                      (index) => Container(
+                        width: 18,
+                        height: 18,
+                        margin: const EdgeInsets.only(right: 4),
+                        decoration: BoxDecoration(
+                          color: product.colors[index],
+                          shape: BoxShape.circle
+                        ),
+                      ))
+                  ,
+                )
+                  ],
+                )
             ],
           ),
         )
